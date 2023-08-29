e156feab08109618fa670c16091839f151a1b522
Amit <amitkumar03022004@gmail.com>
2023-08-30 03:11:22 +0530

    digital clock

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let sec = document.getElementById("sec");
 
  setInterval(()=>{
     let currentDate = new Date();
-    hour.innerHTML = currentDate.getHours();
-    min.innerHTML = currentDate.getMinutes();
-    sec.innerHTML = currentDate.getSeconds();
+    hour.innerHTML = (currentDate.getHours()<10?"0":"") + currentDate.getHours();
+    min.innerHTML = (currentDate.getMinutes()<10?"0":"") + currentDate.getMinutes();
+    sec.innerHTML = (currentDate.getSeconds()<10?"0":"") + currentDate.getSeconds();
  },1000)
